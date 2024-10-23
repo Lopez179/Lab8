a6bfea156d5f666e61004026a72ae091d44576a9
Lopez179 <lflopez@ualberta.ca>
2024-10-23 17:42:08 -0600

    Created Tests

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -25,4 +25,40 @@ public class CustomListTest {
         list.addCity(new City("Estevan", "SK"));
         assertEquals(list.getCount(),listSize + 1);
     }
+
+    @Test
+    void testHasCity() {
+        CustomList cityList = MockCityList();
+        City city1 = new City("Charlottetown", "Prince Edward Island");
+        cityList.add(city1);
+
+        City city2 = new City("Charlottetown", "Maine");
+
+        assertEquals(true, cityList.hasCity(city1));
+        assertEquals(true, cityList.hasCity(city2));
+    }
+
+    @Test
+    void testDeleteCity() {
+        CustomList cityList = MockCityList();
+        City city1 = new City("Charlottetown", "Prince Edward Island");
+        City city2 = new City("Charlottetown", "Maine");
+        cityList.add(city1);
+        cityList.deleteCity(city2);
+        assertEquals(cityList.getCities().size(),1);
+
+        City city3 = new City("Paris", "Ille-de-France");
+        assertThrows(IllegalArgumentException.class, () -> {cityList.deleteCity(city3);});
+    }
+
+    @Test
+    void testCountCities() {
+        CustomList cityList = MockCityList();
+        assertEquals(cityList.CountCities(), 0);
+        City city1 = new City("Charlottetown", "Prince Edward Island");
+        City city2 = new City("Lloydminster", "Alberta");
+        cityList.addCity(city1);
+        cityList.addCity(city2);
+        assertEquals(cityList.CountCities(), 2);
+    }
 }
