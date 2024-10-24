05a603d2d21d54009f068cb5f81138f50ebb8381
Lopez179 <lflopez@ualberta.ca>
2024-10-23 18:32:17 -0600

    Fixed syntax in tests

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -28,36 +28,36 @@ public class CustomListTest {
 
     @Test
     void testHasCity() {
-        CustomList cityList = MockCityList();
+        list = MockCityList();
         City city1 = new City("Charlottetown", "Prince Edward Island");
-        cityList.add(city1);
+        list.addCity(city1);
 
         City city2 = new City("Charlottetown", "Maine");
 
-        assertEquals(true, cityList.hasCity(city1));
-        assertEquals(true, cityList.hasCity(city2));
+        assertEquals(true, list.hasCity(city1));
+        assertEquals(true, list.hasCity(city2));
     }
 
     @Test
     void testDeleteCity() {
-        CustomList cityList = MockCityList();
+        list = MockCityList();
         City city1 = new City("Charlottetown", "Prince Edward Island");
         City city2 = new City("Charlottetown", "Maine");
-        cityList.add(city1);
-        cityList.deleteCity(city2);
-        assertEquals(cityList.getCities().size(),1);
+        list.addCity(city1);
+        list.deleteCity(city2);
+        assertEquals(list.getCities().size(),1);
 
         City city3 = new City("Paris", "Ille-de-France");
     }
 
     @Test
     void testCountCities() {
-        CustomList cityList = MockCityList();
-        assertEquals(cityList.countCities(), 0);
+        list = MockCityList();
+        assertEquals(list.countCities(), 0);
         City city1 = new City("Charlottetown", "Prince Edward Island");
         City city2 = new City("Lloydminster", "Alberta");
-        cityList.addCity(city1);
-        cityList.addCity(city2);
-        assertEquals(cityList.countCities(), 2);
+        list.addCity(city1);
+        list.addCity(city2);
+        assertEquals(list.countCities(), 2);
     }
 }
